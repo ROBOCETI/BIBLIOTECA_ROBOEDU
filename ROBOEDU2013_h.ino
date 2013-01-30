4e49b643171bfb60c746ceb479e939b08aa60b99
Lucas Mizusaki <lepmizusaki@gmail.com>
2013-01-30 19:49:02 -0200

    ArquivisBase

---
+++ ROBOEDU2013_h.ino
@@ -0,0 +1,36 @@
+/*
+   ROBOEDU_2013.h
+   Biblioteca de componentes para o kit de robótica ROBOEDU-2013
+
+
+   Esse código é de domínio público
+*/
+
+
+// Usando o comando Define para facilitar a programação. Cada entrada da placa será numerada
+
+# define E0 0 
+# define E1 1
+# define E2 2
+# define E3 3
+# define E4 4
+# define E5 5
+# define E6 6
+
+// As saídas serão numeradas de 7  a 10
+
+# define S0 7
+# define S1 8 
+# define S2 9 
+# define S3 10
+
+int botao (int entrada);
+int potenciomentro (int entrada);
+
+int LED ();
+
+void setup {}
+
+
+int
+
---
+++ sketch_jan28b/sketch_jan28b.ino
@@ -0,0 +1,16 @@
+/*
+   ROBOEDU_2013.h
+   Biblioteca de componentes para o kit de robótica ROBOEDU-2013
+   Arquivo HEADER
+
+   Esse código é de domínio público
+*/
+
+class ROBOEDU
+{
+   public:
+   
+   private:
+
+
+}
